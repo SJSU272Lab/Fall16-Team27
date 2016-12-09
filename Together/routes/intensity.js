19642562e679630966272ea758acbe141b7d8559
Shrey Bhatt <shreykbhatt@gmail.com>
2016-12-08 17:33:12 -0800

    Get Intensity

--- Together/routes/intensity.js
+++ Together/routes/intensity.js
@@ -4,6 +4,8 @@
 
 var Intensity=require('../model/intensity');
 var mongoose = require('mongoose');
+var Player=require('../model/player');
+var HashMap=require('hashmap');
 
 var ObjectId=require('mongodb').ObjectID;
 
@@ -11,87 +13,194 @@ exports.addIntensity=function(request,response)
 {
     console.log("in addIntensity");
 
-    var newIntensity=new Intensity();
-    newIntensity.playerId= mongoose.Types.ObjectId('5849d3375506fe2f00c19110');
-    newIntensity.date= Date.now();
-    newIntensity.runningIntensityPoint=80;
-    newIntensity.weightingIntensityPoint=65;
-    newIntensity.avgCaloriesRate=200;
-    newIntensity.avgDistanceRate=7;
-    console.log(newIntensity);
 
-    newIntensity.save(function(err,result){
-        console.log(err);
-        response.send(result);
-    });
+    var newIntensity=new Intensity();
+    newIntensity.playerId= mongoose.Types.ObjectId('5849d2a526a28b14c836b2dd');
+    var makeDate=new Date();
+
+        makedate = new Date(makeDate.setDate(makeDate.getDate() - 2));
+        console.log(makeDate);
+        newIntensity.date = makeDate;
+
+        newIntensity.runningIntensityPoint = 100;
+        newIntensity.weightingIntensityPoint = 110;
+        newIntensity.avgCaloriesRate = 110;
+        newIntensity.avgDistanceRate = 9;
+        console.log(newIntensity);
+
+        newIntensity.save(function (err, result) {
+            response.send(result);
+        });
 
 }
 
 exports.getWeightingIntensityData=function(request,response)
 {
 
-    /*Sample JSON Output of weightingIntensityPoint
-     [{"_id":"5849e767f392af00e0b31e95","weightingIntensityPoint":35},
-     {"_id":"5849e7e516d1eb40a0f6ff49","weightingIntensityPoint":45},
-     {"_id":"5849e8fc0f12d937a86cde4b","weightingIntensityPoint":55},
-     {"_id":"5849e931dac0a014dcb8efce","weightingIntensityPoint":45},
-     {"_id":"5849e9625a13b340cce73c27","weightingIntensityPoint":65}]
-*/
     console.log("in weightingIntensityPoint");
+    var currentDate=new Date();
+    var makeDate = new Date();
+    makeDate = new Date(makeDate.setDate(makeDate.getDate() - 7));
+    console.log(Date.parse(currentDate));
+    console.log(Date.parse(makeDate));
+    var resultMap=new HashMap();
+    Intensity.find({})
+        .where('date').gt(makeDate).lte(currentDate)
+        .populate('playerId')
+        .sort('playerId')
+        .exec(function (err,result)
+        {
+            if(!err)
+            {
 
-    Intensity.find({},'weightingIntensityPoint',function (err,intensity)
-    {
-        if(err)
-            console.log(err);
-        console.log(intensity);
-        response.send(intensity);
-    });
+                for(var i=0;i<result.length;i++)
+                {
+                    if(resultMap.has(result[i].playerId.playerName))
+                    {
+                        var tempArr=resultMap.get(result[i].playerId.playerName);
+                        var tempObj=
+                            {
+                                date:result[i].date,
+                                weightingIntensityPoint:result[i].weightingIntensityPoint
+                            }
+                        tempArr.push(tempObj);
+                        resultMap.remove(result[i].playerId.playerName)
+                        resultMap.set(result[i].playerId.playerName,tempArr);
+                    }
+                    else
+                    {
+                        var tempObj=
+                            {
+                                date:result[i].date,
+                                weightingIntensityPoint:result[i].weightingIntensityPoint
+                            }
+                        var tempArr=[];
+                        tempArr.push(tempObj);
+                        resultMap.set(result[i].playerId.playerName,tempArr);
+                    }
+
+                }
+
+                response.send(resultMap);
+            }
+            else
+                response.send({failed:"failed"});
+        });
 
 }
+
 
 
 exports.getRunningIntensityData=function(request,response)
 {
 
-    /*Sample JSON Output of getRunningIntensityData
-     [{"_id":"5849e767f392af00e0b31e95","runningIntensityPoint":20},
-     {"_id":"5849e7e516d1eb40a0f6ff49","runningIntensityPoint":30},
-     {"_id":"5849e8fc0f12d937a86cde4b","runningIntensityPoint":50},
-     {"_id":"5849e931dac0a014dcb8efce","runningIntensityPoint":60},
-     {"_id":"5849e9625a13b340cce73c27","runningIntensityPoint":80}]
 
-     */
-    console.log("in getRunningIntensityData");
+    var currentDate=new Date();
+    var makeDate = new Date();
+    makeDate = new Date(makeDate.setDate(makeDate.getDate() - 7));
+    console.log(Date.parse(currentDate));
+    console.log(Date.parse(makeDate));
+    var resultMap=new HashMap();
+    Intensity.find({})
+        .where('date').gt(makeDate).lte(currentDate)
+        .populate('playerId')
+        .sort('playerId')
+        .exec(function (err,result)
+        {
+            if(!err)
+            {
 
-    Intensity.find({},'runningIntensityPoint',function (err,intensity)
-    {
-        if(err)
-            console.log(err);
-        console.log(intensity);
-        response.send(intensity);
-    });
+                for(var i=0;i<result.length;i++)
+                {
+                    if(resultMap.has(result[i].playerId.playerName))
+                    {
+                        var tempArr=resultMap.get(result[i].playerId.playerName);
+                        var tempObj=
+                            {
+                                date:result[i].date,
+                                runningIntensityPoint:result[i].runningIntensityPoint
+                            }
+                        tempArr.push(tempObj);
+                        resultMap.remove(result[i].playerId.playerName)
+                        resultMap.set(result[i].playerId.playerName,tempArr);
+                    }
+                    else
+                    {
+                        var tempObj=
+                            {
+                                date:result[i].date,
+                                runningIntensityPoint:result[i].runningIntensityPoint
+                            }
+                        var tempArr=[];
+                        tempArr.push(tempObj);
+                        resultMap.set(result[i].playerId.playerName,tempArr);
+                    }
+
+                }
+
+                response.send(resultMap);
+            }
+            else
+                response.send({failed:"failed"});
+        });
 
 }
 
 exports.getAvgCaloriesRateData=function(request,response)
 {
 
-    /*Sample JSON Output of weightingIntensityPoint
-     [{"_id":"5849e767f392af00e0b31e95","avgCaloriesRate":200},
-     {"_id":"5849e7e516d1eb40a0f6ff49","avgCaloriesRate":140},
-     {"_id":"5849e8fc0f12d937a86cde4b","avgCaloriesRate":180},
-     {"_id":"5849e931dac0a014dcb8efce","avgCaloriesRate":170},
-     {"_id":"5849e9625a13b340cce73c27","avgCaloriesRate":200}]
-     */
+
     console.log("in avgCaloriesRate");
 
-    Intensity.find({},'avgCaloriesRate',function (err,intensity)
-    {
-        if(err)
-            console.log(err);
-        console.log(intensity);
-        response.send(intensity);
-    });
+    var currentDate=new Date();
+    var makeDate = new Date();
+    makeDate = new Date(makeDate.setDate(makeDate.getDate() - 7));
+    console.log(Date.parse(currentDate));
+    console.log(Date.parse(makeDate));
+    var resultMap=new HashMap();
+    Intensity.find({})
+        .where('date').gt(makeDate).lte(currentDate)
+        .populate('playerId')
+        .sort('playerId')
+        .exec(function (err,result)
+        {
+            if(!err)
+            {
+
+                for(var i=0;i<result.length;i++)
+                {
+                    if(resultMap.has(result[i].playerId.playerName))
+                    {
+                        var tempArr=resultMap.get(result[i].playerId.playerName);
+                        var tempObj=
+                            {
+                                date:result[i].date,
+                                avgCaloriesRate:result[i].avgCaloriesRate
+                            }
+                        tempArr.push(tempObj);
+                        resultMap.remove(result[i].playerId.playerName)
+                        resultMap.set(result[i].playerId.playerName,tempArr);
+                    }
+                    else
+                    {
+                        var tempObj=
+                            {
+                                date:result[i].date,
+                                avgCaloriesRate:result[i].avgCaloriesRate
+                            }
+                        var tempArr=[];
+                        tempArr.push(tempObj);
+                        resultMap.set(result[i].playerId.playerName,tempArr);
+                    }
+
+                }
+
+                response.send(resultMap);
+            }
+            else
+                response.send({failed:"failed"});
+        });
+
 
 }
 
@@ -99,21 +208,57 @@ exports.getAvgCaloriesRateData=function(request,response)
 exports.getAvgDistanceRateData=function(request,response)
 {
 
-    /*Sample JSON Output of weightingIntensityPoint
-     [{"_id":"5849e767f392af00e0b31e95","avgDistanceRate":7},
-     {"_id":"5849e7e516d1eb40a0f6ff49","avgDistanceRate":6},
-     {"_id":"5849e8fc0f12d937a86cde4b","avgDistanceRate":8},
-     {"_id":"5849e931dac0a014dcb8efce","avgDistanceRate":9},
-     {"_id":"5849e9625a13b340cce73c27","avgDistanceRate":7}]
-     */
     console.log("in getAvgDistanceRateData");
 
-    Intensity.find({},'avgDistanceRate',function (err,intensity)
-    {
-        if(err)
-            console.log(err);
-        console.log(intensity);
-        response.send(intensity);
-    });
+    var currentDate=new Date();
+    var makeDate = new Date();
+    makeDate = new Date(makeDate.setDate(makeDate.getDate() - 7));
+    console.log(Date.parse(currentDate));
+    console.log(Date.parse(makeDate));
+    var resultMap=new HashMap();
+    Intensity.find({})
+        .where('date').gt(makeDate).lte(currentDate)
+        .populate('playerId')
+        .sort('playerId')
+        .exec(function (err,result)
+        {
+            if(!err)
+            {
+
+                for(var i=0;i<result.length;i++)
+                {
+                    if(resultMap.has(result[i].playerId.playerName))
+                    {
+                        var tempArr=resultMap.get(result[i].playerId.playerName);
+                        var tempObj=
+                            {
+                                date:result[i].date,
+                                avgDistanceRate:result[i].avgDistanceRate
+                            }
+                        tempArr.push(tempObj);
+                        resultMap.remove(result[i].playerId.playerName)
+                        resultMap.set(result[i].playerId.playerName,tempArr);
+                    }
+                    else
+                    {
+                        var tempObj=
+                            {
+                                date:result[i].date,
+                                avgDistanceRate:result[i].avgDistanceRate
+                            }
+                        var tempArr=[];
+                        tempArr.push(tempObj);
+                        resultMap.set(result[i].playerId.playerName,tempArr);
+                    }
+
+                }
+
+                response.send(resultMap);
+            }
+            else
+                response.send({failed:"failed"});
+        });
+
+
 
 }
